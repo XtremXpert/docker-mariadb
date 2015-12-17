8a006838e6a5be5bbd768e4bef8854c8927b6b09
XtremXpert <xtremxpert@xtremxpert.com>
2015-12-17 13:10:39 -0500

    revert

--- files/init_mariadb.sh
+++ files/init_mariadb.sh
@@ -3,6 +3,4 @@ mysqld_safe &
 mysqladmin --silent --wait=30 ping || exit 1
 mysql -uroot --execute="CREATE USER 'admin'@'%' IDENTIFIED BY 'admin';"
 mysql -uroot --execute="GRANT ALL PRIVILEGES ON *.* TO 'admin'@'%' WITH GRANT OPTION;"
-mysql -uroot --execute="CREATE USER '$MYSQL_USER'@'%' IDENTIFIED BY '$MYSQL_PASSWORD';"
-mysql -uroot --execute="GRANT ALL PRIVILEGES ON *.* TO '$MYSQL_USER'@'%' WITH GRANT OPTION;"
 mysqladmin reload
